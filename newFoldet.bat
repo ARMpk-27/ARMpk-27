825f6d82e7cadca9b521bd89e84e0971ad72c4ab
ARMpk-27 <armkun2712@gmail.com>
2024-07-30 10:28:40 +0700

    Add files via upload

---
+++ newFoldet.bat
@@ -0,0 +1,6 @@
+mkdir new1 new2 new3
+echo I Love Inet > new1\test1.txt
+echo I Like Inet > new2\test2.txt
+echo I Live Inet > new3\test3.txt
+dir new1 new2 new3
+pause
---
+++ test2.bat
@@ -0,0 +1,5 @@
+@echo off
+rem mkdir TestFolder
+rem echo some text > TestFolder\testFile.txt
+dir TestFolder
+pause
